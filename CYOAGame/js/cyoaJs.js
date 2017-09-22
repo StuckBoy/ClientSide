ea690e3d67108d8d52ccab7e2d0c93ceeacd5c4e
StuckBoy <zstuck@nmu.edu>
2017-09-22 09:21:08 -0400

    Extended storyline and added bad path.

--- CYOAGame/js/cyoaJs.js
+++ CYOAGame/js/cyoaJs.js
@@ -10,12 +10,16 @@ var philosophyGreeting = "Welcome to the College of Philosophy!";
 var poliSciGreeting = "Welcome to the College of Political Science!";
 var businessGreeting = "Welcome to the College of Business!";
 var constructionGreeting = "Welcome to the College of Construction!";
-var genericWelcome = "We are glad you chose our school as your school of choice. We need you to declare your major as soon as possible for our records. You can always change it later on, and we've even taken the liberty of listing our most popular majors here!";
+var genericWelcome = " We are glad you chose our school as your school of choice. We need you to declare your major as soon as possible for our records. You can always change it later on, and we've even taken the liberty of listing our most popular majors here!";
 var badFinal = "After successfully finishing your degree, you head out into the great wide open. After many months of unsuccessful job searching in your field, you settle down in Starbucks in the metropolitan area. Hopefully something good comes along soon...";
 var goodFinal = "While it took much trial and tribulation, you managed to successfully complete your degree. With new-found knowledge in hand, you head out into the workforce. After a couple of interviews that turned up fruitless, you finally manage to land a job at a pretty reputable workplace. The future sure is looking bright!";
+var examTime = "As the years go by you quickly find yourself in your senior year of college. It's been a typical experience; finding new friends, broadening your mind, and learning more in the field of your choosing. It is currently exam week, and you still have two left to go. To unwind, you decided to spend some time with friends reminiscing about previous years. Around 11pm, you decide you should head off to bed, seeing as you have an exam in the morning. Your friends complain that staying up just another hour won't kill you, but you may be too tired for the test."
+var overslept = "You decided to stay up another hour to appease your friends. Unfortunately when you awoke the next morning you find that you have overslept by an hour. You rush to grab your things and run up campus to take your exam. With no time to prep in the morning you don't feel very confident about this test. Unfortunately, it did not turn out well. There's always next semester though!"
+var wellRested = "You told your friends that there was always time tomorrow to hang out again, and that you can't afford to fail this exam. Upon waking up you feel well rested and extremely confident about this exam. "
 var decisionNum = 0;
 var buttonClicked = "";
-var buttonText = ""
+var buttonText = "";
+var degreeAnswer = "";
 
 function linkButtons() {
    $("button").click(
@@ -30,7 +34,7 @@ function linkButtons() {
             majorChoice(buttonClicked, buttonText);
          }
          else {
-            finalDecision(buttonClicked, buttonText);
+            finalDecision(buttonClicked, buttonText, degreeAnswer);
          }
       }
    );
@@ -63,30 +67,44 @@ function schoolChoice(buttonId) {
 }
 
 function majorChoice(buttonId, buttonText) {
-  console.log("Major Choice Time!");
-  var buttonOne = document.getElementById("B1").innerHTML;
-  var buttonTwo = document.getElementById("B2").innerHTML;
-  console.log(buttonOne, buttonTwo, buttonText);
-  if (buttonText == "Art" || buttonText == "Music" || buttonText == "Philosophy") {
-    document.getElementById('mainTextArea').innerHTML = badFinal;
-    document.getElementById('mainTextArea').background = 'url(img/sadEnd.jpg)';
-    }
-  else {
-    document.getElementById('mainTextArea').innerHTML = goodFinal;
+   console.log("Exam Time!");
+   var buttonOne = document.getElementById("B1").innerHTML;
+   var buttonTwo = document.getElementById("B2").innerHTML;
+   console.log(buttonOne, buttonTwo, buttonText);
+   if (buttonText == "Computer Science") {
+     var urlString = 'img/ComputerScience.jpg';
+   }
+   else if (buttonText == "Political Science") {
+     var urlString = 'img/PoliticalScience.jpg';
+   }
+   else {
+     var urlString = 'img/' + buttonText + '.jpg';
+   }
+   document.getElementById('div2').innerHTML = '<img src = ' + urlString + ' width="350" height="300"/>';
+   degreeAnswer = buttonText;
+   document.getElementById('mainTextArea').innerHTML = examTime;
+   document.getElementById("B1").innerHTML = "Go to bed";
+   document.getElementById("B2").innerHTML = "Stay up another hour";
+   decisionNum++;
+}
 
+function finalDecision(buttonId, buttonText, degreeAnswer) {
+  console.log("Results Time!");
+  console.log(buttonText, degreeAnswer);
+  if (buttonText == "Stay up another hour") {
+     document.getElementById('mainTextArea').innerHTML = overslept;
+     document.body.style.background = "gray";
   }
-  if (buttonText == "Computer Science") {
-    var urlString = 'img/ComputerScience.jpg';
-  }
-  else if (buttonText == "Political Science") {
-    var urlString = 'img/PoliticalScience.jpg';
-  }
+  else if (degreeAnswer == "Art" || degreeAnswer == "Music" || degreeAnswer == "Philosophy") {
+    document.getElementById('mainTextArea').innerHTML = wellRested + badFinal;
+    var urlString2 = 'img/diploma.jpg';
+    document.getElementById('div3').innerHTML = '<img src = ' + urlString2 + ' width="350" height="300"/>';
+   }
   else {
-    var urlString = 'img/' + buttonText + '.jpg';
+    document.getElementById('mainTextArea').innerHTML = wellRested + goodFinal;
+    var urlString2 = 'img/diploma.jpg';
+    document.getElementById('div3').innerHTML = '<img src = ' + urlString2 + ' width="350" height="300"/>';
   }
-  document.getElementById('div2').innerHTML = '<img src = ' + urlString + ' width="350" height="300"/>';
-  var urlString2 = 'img/diploma.jpg';
-  document.getElementById('div3').innerHTML = '<img src = ' + urlString2 + ' width="350" height="300"/>';
   $("button").remove();
   document.getElementById('question').innerHTML = "";
 }
